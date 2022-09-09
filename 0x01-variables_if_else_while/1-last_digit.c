120a6add22d9801ff633ad7069c839e0c94ebe53
Eke Nkechi <alfredpatience03@gmail.com>
2022-09-09 12:21:35 -0700

    just say it

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-	char low;
+	int n;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	       	printf("\n");
-
-		return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
