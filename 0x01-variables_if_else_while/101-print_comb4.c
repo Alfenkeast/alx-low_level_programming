adce5468842871d892966460bcfaca280972244e
Eke Nkechi <alfredpatience03@gmail.com>
2022-09-12 12:37:19 -0700

    i can

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - print numbers from 00 to 99.
+* main - print numbers from 00 to 99.
 *
 * Return: 0 on success
 */
@@ -11,3 +11,34 @@ int main(void)
 	int k = '0';
 
 	while (i <= '7')
+	{
+		while (j <= '8')
+		{
+
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k =='9'))
+					{
+						putchar(',');
+						putchar(' ');	
+					
+					}
+				
+				}
+				k++;
+			
+			}
+			k = '0';
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
