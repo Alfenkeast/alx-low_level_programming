404f169a41f8d461220f5e53e2232bbe568d69f8
Eke Nkechi <alfredpatience03@gmail.com>
2022-09-13 13:36:23 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,11 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 1; mult <=9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			
-
 			prod = num * mult;
 
 			if (prod <= 9)
