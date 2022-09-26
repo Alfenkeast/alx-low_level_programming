c62ace37ce2687e8731cc0e25269a11983576276
Eke Nkechi <alfredpatience03@gmail.com>
2022-09-26 09:20:41 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,6 +2,7 @@
 
 /**
  * _memcpy - copies that will recieve the copied data
+ *
  * @dest: point that will recieve the copied data
  * @src: pointer that has the information to be copied
  * @n: amount of times the src information will be coppied to dest
@@ -12,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
