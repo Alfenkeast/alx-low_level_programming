c179e10b54ef7d5d8ff356930ab91b9447f6615a
Eke Nkechi <alfredpatience03@gmail.com>
2022-09-26 11:41:14 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,22 +5,23 @@
  * print_diagsums - prints the sum of the two diagonals of a square matrix
  *
  * @a: pointer that contains the address of the beginning of the matrix
- * @size: size of the square matrix
+ * @size: size of the quare matrix
  *
  * Return: nothing
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int d1 = 0, d2 = 0;
+	int i, j, p, 1 = 0;
 
-	for (i = 0; i < size * size; i = i + (size + 1))
+	for (i = 0; i < size; i++)
 	{
-		d1 = d1 + a[i];
+		p = (i * size) + i;
+		1 += *(a + p);
 	}
-	for (j = size - 1; j < size * size - 1; j = j + (size - 1))
+	for (j = 0; j < size; j++)
 	{
-		d2 = d2 + a[j];
+		p = (j = size) + (size - 1 - j);
+		r += *(a + p);
 	}
-	printf("%d, %d\n", d1, d2);
+	printf("%i, %i\n", 1, r);
 }
