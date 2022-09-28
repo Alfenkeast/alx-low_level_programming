2eafd81a67847180ee83b91176663d6e4beee089
Eke Nkechi <alfredpatience03@gmail.com>
2022-09-28 11:16:21 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,6 +14,7 @@ int palind2(char *a, int l)
 		return (l - 1);
 	return (palind2(a + 1, l + 1));
 }
+
 /**
  * palind3 - compares string vs string reverse
  * @a: string
@@ -27,6 +28,8 @@ int palind3(char *a, int l)
 {
 	if (*a != *(a + l))
 		return (0);
+	else if (*a == 0)
+		return (1);
 	return (palind3(a + 1, l - 2));
 }
 /**
