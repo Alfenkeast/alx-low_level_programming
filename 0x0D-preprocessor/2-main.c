094891065efe1cfa97b5fdffc35d1e63b971f86c
Eke Nkechi <alfredpatience03@gmail.com>
2022-10-11 07:48:59 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Alx School students.
+ *
+ * Return: 1 if an error occurred, 0 otherwise
+ */
+int main(void)
+{
+	print("%\n", _FILE_);
+	return (0);
+}
