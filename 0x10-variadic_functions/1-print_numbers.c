9fdf0230ab279d4d9f0bf1180b2fdefff6545684
Alfenkeast <110989525+Alfenkeast@users.noreply.github.com>
2022-10-19 10:39:33 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 /**
- * print_numbers - check the code for Holberton School students.
+ * print_numbers - check the code for ALX School students.
  * @n: number of arguments
  * @separator: character separator of numbers
  * Return: Always 0.
